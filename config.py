fb1374a95c5ca49f9ace2206f6efb4257c60cb31
Kyle Sayers <kylesayrs@gmail.com>
2023-03-19 00:01:55 -0700

    stable 10 string

--- config.py
+++ config.py
@@ -3,25 +3,25 @@ from pydantic import BaseModel, Field, validator
 
 class Config(BaseModel):
     # environment
-    STRING_LENGTH: int = Field(default=7)
+    STRING_LENGTH: int = Field(default=10)
 
     # policy
     POLICY_EPSILON: float = Field(default=0.7)
     POLICY_EPSILON_MAX: float = Field(default=1.0)
 
     # simulation
-    NUM_EPISODES: int = Field(default=20000)
-    MAX_EPISODE_STEPS: int = Field(default=9)  # spend longer on harder goals
-    REPLAY_BUFFER_SIZE: int = Field(default=256)
+    NUM_EPISODES: int = Field(default=7000)
+    MAX_EPISODE_STEPS: int = Field(default=15)  # spend longer on harder goals
+    REPLAY_BUFFER_SIZE: int = Field(default=512)
 
     # HER
     HER_ENABLED: bool = Field(default=True)
-    HER_MAX_DISTANCE: int = Field(default=0)
+    HER_MAX_DISTANCE: int = Field(default=4)
 
     # optimization
-    LEARNING_RATE: float = Field(default=0.03)
-    BATCH_SIZE: int = Field(default=64)
-    GAMMA: float = Field(default=0.8)  # coefficient on future q values
+    LEARNING_RATE: float = Field(default=0.08)
+    BATCH_SIZE: int = Field(default=256)
+    GAMMA: float = Field(default=0.95)  # coefficient on future q values
     DQN_MOMENTUM: float = Field(default=0.51)  # per episode
     NUM_EVAL_EPISODES: int = Field(default=500)
 
